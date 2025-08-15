200b7f6785fda852b99d9d8a5bf074e8467f8e22
ASIS MONDAL <165210120+ImA5i5@users.noreply.github.com>
2025-08-16 03:58:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+require("dotenv").config()
+const express=require("express");
+const dbConn=require("./app/config/dbConn");
+const session = require('express-session');
+const flash = require('connect-flash');
+const path=require("path");
+const app=express()
+dbConn()
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.set("view engine","ejs")
+app.set("views","views")
+app.use(express.static(path.join(__dirname,"public")))
+app.use("/uploads", express.static("uploads"));
+app.use(session({
+  secret: 'asis12345',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+
+
+
+
+const adminRouter=require("./app/router/adminRouter")
+app.use(adminRouter)
+
+
+
+const CustomerRouter=require("./app/router/CustomerRouter");
+app.use(CustomerRouter)
+
+
+
+
+const port=process.env.PORT || 5005
+app.listen(port,()=>{
+    console.log(`app is running on port ${port}`)
+})
